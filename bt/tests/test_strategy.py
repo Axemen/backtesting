5eca6f366a9658ab9e5b0412eb0d0baf8564b0f4
Axemen <Jrandolph011@gamil.com>
2021-07-19 21:53:38 -0500

    adding pytest fixture to not share scope across tests

--- bt/tests/test_strategy.py
+++ bt/tests/test_strategy.py
@@ -1,19 +1,20 @@
 from functools import partial
+import os
 
 import pytest
 import numpy as np
 import pandas as pd
+import plotly.graph_objects as go
 
 from ..strategy import Strategy, Signal
 from .. import indicators
-from bt import strategy
 
 data = pd.Series(
     [np.random.randint(1, 100) for _ in range(100)], name="close"
 ).to_frame()
 
 
-class TestStrategy(Strategy):
+class ExampleStrategy(Strategy):
     """
     A Simple moving average strategy example
     """
@@ -37,74 +38,83 @@ class TestStrategy(Strategy):
         return self.data.sma_short.iloc[-1] < self.data.sma_long.iloc[-1]
 
 
+@pytest.fixture(scope="function")
+def strategy():
+    data = pd.Series(
+        [np.random.randint(1, 100) for _ in range(100)], name="close"
+    ).to_frame()
+    strategy = ExampleStrategy(data)
+    return strategy, data
+
+
 def test_init():
 
     # Should work with a pandas.Series and a pandas.DataFrame
-    strategy = TestStrategy(data)
-    strategy = TestStrategy(data.close)
+    strategy = ExampleStrategy(data)
+    strategy = ExampleStrategy(data.close)
 
     # When given the start_index parameter as None, it should default to the first index
     # in the data
-    strategy = TestStrategy(data, start_index=None)
+    strategy = ExampleStrategy(data, start_index=None)
     assert strategy._start_index == data.index[0]
 
     # When given the start_index parameter it must be the same type
     # as the index of the data
     with pytest.raises(TypeError):
-        strategy = TestStrategy(data, start_index="This is a string")
+        strategy = ExampleStrategy(data, start_index="This is a string")
 
 
-def test_add_indicator():
-    strategy = TestStrategy(data)
+def test_add_indicator(strategy):
+    strat, data = strategy
 
     # Reset the indicators
-    strategy._indicator_functions = []
+    strat._indicator_functions = []
 
     # Should append the (name, function) tuple to the self._indicator_functions list
     sma = partial(indicators.sma, window=10)
-    strategy.add_indicator(indicator=sma, name="sma_short")
+    strat.add_indicator(indicator=sma, name="sma_short")
 
-    assert strategy._indicator_functions == [("sma_short", sma)]
+    assert strat._indicator_functions == [("sma_short", sma)]
 
 
-def test_data():
-    strategy = TestStrategy(data)
+def test_data(strategy):
+    strat, data = strategy
 
     # The data property should show all the data if the strategy is not currently backtesting
     # Otherwise it will only show the data that is currently viewable to the backtest
-    assert strategy.data.equals(data)
+    assert strat.data.equals(data)
 
     # When the strategy has viewable data, it should return the viewable data instead
     # of the full data
-    strategy = TestStrategy(data, start_index=10)
+    strat = ExampleStrategy(data, start_index=10)
 
     # Line pulled from Strategy.backtest
-    strategy._viewable_data = strategy._all_data.loc[: strategy._start_index]
+    strat._viewable_data = strat._all_data.loc[: strat._start_index]
 
-    assert strategy.data.equals(strategy._viewable_data)
+    assert strat.data.equals(strat._viewable_data)
 
 
-def test_calculate_indicators():
-    strategy = TestStrategy(data)
+def test_calculate_indicators(strategy):
+    strat, data = strategy
 
     # Manually calculate the indicators (This is usually done by the backtest)
-    strategy._calculate_indicators()
+    strat._calculate_indicators()
 
     # Ensure that the indicators are calculated correctly
     values = indicators.sma(data["close"], window=20)
-    assert strategy._all_data.sma_short.equals(values)
+    assert strat._all_data.sma_short.equals(values)
 
     # Ensure that the indicators are named correctly
-    assert strategy.data.sma_short.name == "sma_short"
+    assert strat.data.sma_short.name == "sma_short"
 
     # Ensure that the indicators are calculated correctly with a dataframe
 
     values = indicators.macd(data["close"])
-    assert strategy._all_data[values.columns].equals(values)
+    assert strat._all_data[values.columns].equals(values)
 
 
 def test_get_signal():
-    class TestStrategy(Strategy):
+    class ExampleStrategy(Strategy):
         def __init__(self, data: pd.Series, start_index=None) -> None:
             super().__init__(data, start_index)
 
@@ -115,22 +125,23 @@ def test_get_signal():
             return self.data.close.iloc[-1] < self.data.close.iloc[-2]
 
     data = pd.Series([1, 2, 3])
-    strategy = TestStrategy(data)
+    strategy = ExampleStrategy(data)
     assert strategy.get_signal() == Signal.BUY
 
     data = pd.Series([3, 2, 1])
-    strategy = TestStrategy(data)
+    strategy = ExampleStrategy(data)
     assert strategy.get_signal() == Signal.SELL
 
     data = pd.Series([1, 1, 1])
-    strategy = TestStrategy(data)
+    strategy = ExampleStrategy(data)
     assert strategy.get_signal() == Signal.HOLD
 
 
-def test_backtest():
-    strategy = TestStrategy(data, start_index=30)
+def test_backtest(strategy):
+    # strategy = ExampleStrategy(data, start_index=30)
+    strat, data = strategy
 
-    results = strategy.backtest(initial_balance=1000)
+    results = strat.backtest(initial_balance=1000)
 
     # Check the results exist
     assert results
@@ -146,5 +157,22 @@ def test_backtest():
     # make sure that the len of porfolio_balance is equal to the len of the data
     # being backtested over
     assert len(results["portfolio_balance"]) == len(
-        strategy._all_data[strategy._start_index :]
+        strat._all_data[strat._start_index :]
     )
+
+
+def test_plot_results(strategy):
+    strat, data = strategy
+
+    # This should raise a ValueError if the strategy has not been backtested
+    with pytest.raises(ValueError):
+        strat.plot_results()
+
+    results = strat.backtest(initial_balance=1000)
+
+    # Check that the internal resutls exist
+    assert strat._results
+
+    # Should return a plotly figure
+    fig = strat.plot_results()
+    assert isinstance(fig, go.Figure)

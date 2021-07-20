426270d410950fbf0857da0da1edc07c16ce730a
Axemen <Jrandolph011@gamil.com>
2021-07-19 22:48:54 -0500

    updating docs

--- bt/util.py
+++ bt/util.py
@@ -2,6 +2,8 @@ def pct_change(initial, final):
     """
     Calculate the percentage change between two numbers.
 
+    ((final - initial) / initial) * 100
+
     :param initial: The first number.
     :param final: The second number.
     :return: The percentage change between a and b.
